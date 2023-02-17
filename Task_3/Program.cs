93b418ddb84f72d11195171d095f410f458ee030
Сергей Челпанов <S.inter.4@mail.ru>
2023-02-17 16:45:39 +0300

    Задача 3.1

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -13,14 +13,14 @@ double RandomDouble(int minBorder, int maxBorder)
 double[] RandomArray(int lengt, int minBorder, int maxBorder)
 {
     double[] array = new double[lengt];
-    for (int i = 1; i < lengt; i++) array[i] = RandomDouble(minBorder, maxBorder);
+    for (int i = 0; i < lengt; i++) array[i] = RandomDouble(minBorder, maxBorder);
     return array;
 }
 
 void SumMinMax(double[] array)
 {
     double min = array[0], max = array[0], total = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 1; i < array.Length; i++)
     {
         if (array[i] < min) min = array[i];
         else if (array[i] > max) max = array[i];
@@ -30,6 +30,6 @@ void SumMinMax(double[] array)
     Console.WriteLine($"min + max = {total}");
 }
 
-double[] randomArray = RandomArray(10, 1, 10);
+double[] randomArray = RandomArray(5, 1, 10);
 Console.WriteLine(string.Join(" ", randomArray));
 SumMinMax(randomArray);
